c938f92301fcf3e5993ddea1178e68986d8522f2
laugek <lauge.kongstad@gmail.com>
2017-06-08 22:50:34 +0200

    started on getGrades.py, getting the urls for grabbing data seems to work..

--- getGrades.py
+++ getGrades.py
@@ -1,1 +1,109 @@
+#! /usr/bin/python3
 
+### ^ 'shebang' for running with bash ("./getGrades_DTU.py" instead of "python3 getGrades_DTU.py") ^ ###
+
+'''
+What?
+	Get grades from nosql database with courses
+Result?
+	Big Dollar Data Cloud ftw
+'''
+
+##################################################################################################################
+################################################ LIBRARIES #######################################################
+##################################################################################################################
+
+import json, time, re
+from datetime import datetime
+import pymongo
+from pymongo import MongoClient
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import pprint
+##################################################################################################################
+################################################ UTILITIES #######################################################
+##################################################################################################################
+
+### Initialize database-connection:
+client = MongoClient("mongodb://admin:datbat333@localhost:27017/tracking?authSource=admin")
+# database
+db = client['DTU']
+# existing collection with courses
+courseColl = db['course_info']
+# the new collection with grades
+gradeColl = db['grades']
+
+# baseurl of grade sites..
+#example: http://karakterer.dtu.dk/Histogram/1/62612/Winter-2017
+baseUrlGrade = 'http://karakterer.dtu.dk/Histogram/1/'
+# baseurl of list of semesters available site
+#example: http://kurser.dtu.dk/course/02162/info
+baseUrlSem = 'http://kurser.dtu.dk/course/'
+##################################################################################################################
+################################################ MAIN-METHODS #####################################################
+##################################################################################################################
+
+
+##################################################################################################################
+################################################ SUB-METHODS #####################################################
+##################################################################################################################
+### Method: getting available semesters to crawl...
+def getSemesters(course):
+    url = baseUrlSem+course['course_id']+'/info'
+    print('\n', url, '\n')
+    xpath = "//*[@class='row']//*[@class='col-md-6']//*[@class='box']/div"
+    driver.get(url)
+    time.sleep(2)
+    # the url contains two types of hrefs pointing to evaluations and grades of
+    # course in different semesters
+    value = []
+    hrefGrades = []
+    hrefEval = []
+    #we also want to store the acutal semesters of grades and evaluations..
+    semesterGradeList = []
+    semesterEvalList = []
+    # finding the stuff...
+    value = driver.find_elements_by_xpath(xpath)
+    for elem in value:
+        try:
+            elem = elem.find_element_by_css_selector('a').get_attribute('href')
+            if "karakterer" in elem:
+                print("Found grade href:", elem)
+                hrefGrades.append(elem)
+                semesterGradeList.append(elem.rsplit('/', 1)[-1])
+            elif "evaluering" in elem:
+                print("Found evaluation href:", elem)
+                hrefEval.append(elem)
+                semesterEvalList.append(elem.rsplit('/', 1)[-1])
+            else:
+                print("Found an href but neither an evaluation or a grade?")
+        except:
+            print('Couldnt find any href in element all!')
+            #print('Couldnt find any href at all! Found instead:', elem.text)
+    #print('\nLength of value =',len(value) )
+    #print('Length of hrefGrades =',len(hrefGrades) )
+    #print('Length of semesterList =',len(semesterList) )
+    #print('Length of hrefEval =',len(hrefEval) )
+    #print(semesterList)
+    return(hrefEval, semesterEvalList, hrefGrades, semesterGradeList)
+
+    
+### Method: Initializiation method for a webdriver-object
+def init_driver():
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+    driver.wait = WebDriverWait(driver, 2)
+    driver.maximize_window()
+    return(driver)
+
+### Method: Initialization-point of the script. Put main methods here.
+if __name__ == "__main__":
+    driver = init_driver()
+    course = list(courseColl.find())[0:2]
+    for elem in course:
+        getSemesters(elem)
+    time.sleep(1)
+    driver.close()
+
+# http://selenium-python.readthedocs.io/locating-elements.html
+print ("Reached end of doc!!")
--- playRound.py
+++ playRound.py
@@ -32,17 +32,13 @@ post = {"author": "Mike",
 post_id = posts.insert_one(post).inserted_id
 
 # finding with the find_one() command
-print "printing the first match with 'find_one()'' command in the 'posts':"
+print ("printing the first match with 'find_one()'' command in the 'posts':")
 pprint.pprint(posts.find_one())
-print ""
-print "we can find the first post from mike in 'posts':"
+print ("we can find the first post from mike in 'posts':")
 pprint.pprint(posts.find_one({"author": "Mike"}))
-print""
-print "but none from elliot:"
+print ("but none from elliot:")
 pprint.pprint(posts.find_one({"author": "elliot"}))
-print""
-print""
-print"or locate from the post_id:"
+print ("or locate from the post_id:")
 pprint.pprint(posts.find_one({"_id": post_id}))
 
 # inserting multiple documents
@@ -56,47 +52,36 @@ new_posts = [{"author": "Mike", # first
     "date": datetime.datetime(2009, 11, 10, 10, 45)}]
 
 result = posts.insert_many(new_posts)
-print ""
-print "the ids of the inserted new_posts - there is two now, not one:"
-print result.inserted_ids
+print ("the ids of the inserted new_posts - there is two now, not one:")
+print (result.inserted_ids)
 
 # finding more than one document with loop - here all..:
-print ""
-print ""
-print "finding all documents with find() command"
+print ("finding all documents with find() command")
 for post in posts.find():
     pprint.pprint(post)
-print "total number of posts:"
-print posts.count()
+print ("total number of posts:")
+print (posts.count())
 
 # finding more than one document with loop - here all from mike..:
-print ""
-print ""
-print "finding all documents from mike with find() command"
+print ("finding all documents from mike with find() command")
 for post in posts.find({"author": "Mike"}):
     pprint.pprint(post)
-print "total number of posts from Mike:"
-print posts.count({"author": "Mike"})
+print ("total number of posts from Mike:")
+print (posts.count({"author": "Mike"}))
 
 # advanced queries
 # say we have a date 'd',  older posts are of interest
 d = datetime.datetime(2009, 11, 12, 12)
-print ""
-print ""
-print "Posts older than a date 'd', sorted by author:"
+print ("Posts older than a date 'd', sorted by author:")
 for post in posts.find({"date": {"$lt": d}}).sort("author"):
     pprint.pprint(post)
 
 #something with userids...
-print ""
-print ""
-print "We have created a user_id, so now there is two different indexes:"
+print ("We have created a user_id, so now there is two different indexes:")
 asdfasdfasdf = db.profiles.create_index([('user_id', pymongo.ASCENDING)],unique=True)
-print sorted(list(db.profiles.index_information()))
+print (sorted(list(db.profiles.index_information())))
 
 # setting up userprofilse
-print ""
-print ""
 user_profiles = [
     {'user_id': 211, 'name': 'Luke'},
     {'user_id': 212, 'name': 'Ziltoid'}]
@@ -105,8 +90,6 @@ db.profiles.insert_many(user_profiles)
 new_profile = {'user_id': 213, 'name': 'Drew'}
 duplicate_profile = {'user_id': 212, 'name': 'Tommy'}
 db.profiles.insert_one(new_profile)  # This is fine.
-# db.profiles.insert_one(duplicate_profile) # this gives error 
+# db.profiles.insert_one(duplicate_profile) # this gives error
 
-print ""
-print "Succes!! Reached end of document.."
-print ""
+print ("Succes!! Reached end of document..")
